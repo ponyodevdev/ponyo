42807e759c8386340264437a24c662c1640204a7
ponyodevdev <106717441+ponyodevdev@users.noreply.github.com>
2023-11-27 13:51:27 +0900

    소멘텀

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*
+당신의 이름을 적어 주세요.라고 사용자에게 물어 답변을 화면에 표시한다. 
+*/
+
+//document의 id가 frmLogin인 form
+const frmLogin = document.getElementById("formLogin");
+
+
+//inputbox
+const inputLogin = document.qurySelctor("#frmLogin input");
+
+console.log()
+
+//login 버튼으로 구성
+
+console.log(frmLogin);
